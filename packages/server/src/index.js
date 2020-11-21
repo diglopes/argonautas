9a74aa16316c104ca76ef36c8dfc5146c9ffb617
Diego Lopes <diego-chez@hotmail.com>
2020-11-21 07:51:41 -0300

    add basic express app

---
+++ packages/server/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors")
+const routes = require("./routes");
+const app = express();
+
+const APP_PORT = process.env.APP_PORT || 3000;
+
+app.use(cors())
+app.use(routes);
+
+app.listen(APP_PORT, () => {
+  console.log("App running on port", APP_PORT);
+});
