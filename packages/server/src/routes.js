be666b193d088d0f1af21281159375498ea0ea9d
Diego Lopes <diego-chez@hotmail.com>
2020-11-22 03:46:27 -0300

    add exec promise

--- packages/server/src/routes.js
+++ packages/server/src/routes.js
@@ -4,7 +4,7 @@ const IA = require("./scripts/AI")
 const routes = Router()
 
 routes.get("/estimarPraticagem", async (req, res) => {
-    const {areaFundeio, idTrajeto} = req.query
+    const { areaFundeio, idTrajeto } = req.query
     const tempoEstimado = await IA.run(areaFundeio, idTrajeto)
     res.send({ tempoEstimado })
 })
--- packages/server/src/scripts/AI.js
+++ packages/server/src/scripts/AI.js
@@ -1,3 +1,7 @@
+const { exec } = require('child_process')
+const { promisify } = require('util')
+const execPromisified = promisify(exec)
+
 async function run(areaFundeio, idTrajeto) {
     const res = await execPromisified(`python3 -W ignore /home/ubuntu/nn_MPerceptron_Tprat_extimation_past_2.py ${areaFundeio} ${idTrajeto}`)
     console.log(Number(Number(res.stdout)));
