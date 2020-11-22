c54bcf3b822dfeedbabf2de4811a7fddfbea5bff
Diego Lopes <diego-chez@hotmail.com>
2020-11-22 03:55:59 -0300

    add express json

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 const APP_PORT = process.env.APP_PORT || 3000;
 
+app.use(express.json())
 app.use(cors())
 app.use(routes);
 
--- packages/server/src/routes.js
+++ packages/server/src/routes.js
@@ -6,7 +6,7 @@ const routes = Router()
 routes.get("/estimarPraticagem", async (req, res) => {
     const { areaFundeio, idTrajeto } = req.query
     const tempoEstimado = await IA.run(areaFundeio, idTrajeto)
-    res.send({ tempoEstimado })
+    res.status(200).json({ tempoEstimado })
 })
 
 module.exports = routes
