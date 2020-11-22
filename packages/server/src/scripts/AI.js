551f2afaec84747b87b56888e36be213d247e830
Diego Lopes <diego-chez@hotmail.com>
2020-11-22 03:50:56 -0300

    fix ia path

--- packages/server/src/scripts/AI.js
+++ packages/server/src/scripts/AI.js
@@ -1,9 +1,10 @@
 const { exec } = require('child_process')
 const { promisify } = require('util')
 const execPromisified = promisify(exec)
+const path = 
 
 async function run(areaFundeio, idTrajeto) {
-    const res = await execPromisified(`python3 -W ignore /home/ubuntu/nn_MPerceptron_Tprat_extimation_past_2.py ${areaFundeio} ${idTrajeto}`)
+    const res = await execPromisified(`python3 -W ignore /home/ubuntu/ann_MPerceptron_Tprat_extimation_past_2.py ${areaFundeio} ${idTrajeto}`)
     console.log(Number(Number(res.stdout)));
 }
 
