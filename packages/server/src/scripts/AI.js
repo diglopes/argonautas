7a2cdc73920152e4331379085f57fff7cb765192
Diego Lopes <diego-chez@hotmail.com>
2020-11-22 03:52:49 -0300

    remove path

--- packages/server/src/scripts/AI.js
+++ packages/server/src/scripts/AI.js
@@ -1,7 +1,6 @@
 const { exec } = require('child_process')
 const { promisify } = require('util')
 const execPromisified = promisify(exec)
-const path = 
 
 async function run(areaFundeio, idTrajeto) {
     const res = await execPromisified(`python3 -W ignore /home/ubuntu/ann_MPerceptron_Tprat_extimation_past_2.py ${areaFundeio} ${idTrajeto}`)
